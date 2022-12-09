24123fd91417b51be2b99315dda1d8a9dc4ebffe
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-12-09 16:39:18 +0530

    phone1: Switch to legacy-um sepolicy_vndr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include $(DEVICE_PATH)/sepolicy/SEPolicy.mk
 
 # Security
