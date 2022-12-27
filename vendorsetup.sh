21305a0d3599b81580f911baf580c7ae2b02600d
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-12-27 14:34:23 +0530

    Spacewar: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Dhruvesh16/vendor_nothing_phone1 vendor/nothing/phone1
+git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325.git -b thirteen kernel/nothing/sm7325
