effa2d7efe5f2ced2e037233d0aa536a7743af5b
dhruvesh16 <dhruvesh3466@protonmail.com>
2023-10-05 16:01:24 +0530

    Revert "phone1: overlay: Enable BoostFramework"
    This reverts commit 4e27b6f5bd606e11514f44434e14d4fca6d19827.

--- device.mk
+++ device.mk
@@ -308,10 +308,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-# RRO (derp)
-PRODUCT_PACKAGES += \
-    BoostFrameworkOverlay
-
 # Overlay AOSP
 PRODUCT_PACKAGES += \
     AOSPFrameworks \
