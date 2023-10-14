ProjectElixir-Devices/device_nothing_Spacewar

Spacewar: sepolicy: Allows glyphs app to get powershare status
Change-Id: Iaa196f2a8d744c3beafa60b7be6749d58fac8136
Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

Conflicts:
	sepolicy/vendor/file.te

sepolicy/SEPolicy.mk
@@ -5,4 +5,5 @@
 #
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/private

Dhruvesh16/device_nothing_phone1

sepolicy/SEPolicy.mk
@@ -5,4 +5,5 @@
 #
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/nothing/phone1/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/nothing/phone1/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/nothing/phone1/sepolicy/private