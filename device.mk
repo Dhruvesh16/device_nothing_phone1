7dd3b6893a8910c6150db5e22341d569669a4b4e
luk1337 <priv.luk@gmail.com>
2024-06-01 10:04:04 +0530

    phone1: Add libcamera_metadata shim for stripping out system camera…… cap

--- device.mk
+++ device.mk
@@ -128,6 +128,7 @@ $(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera_metadata_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
         vendor/lib64/libgf_hal.so)
             sed -i "s|ro.boot.flash.locked|ro.bootloader.locked|g" "${2}"
             ;;
+        vendor/lib64/hw/com.qti.chi.override.so)
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
+            ;;
     esac
 }
 
