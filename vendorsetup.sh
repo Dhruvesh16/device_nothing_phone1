622edc455cef2fa1258fff240ef8b9f5b7ba2867
dhruvesh16 <dhruvesh3466@protonmail.com>
2024-07-23 17:20:55 +0530

    phone1: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/Dhruvesh16/vendor_nothing_phone1 -b 14  vendor/nothing/phone1
-git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325.git -b fourteen-v2 kernel/nothing/sm7325
-git clone https://gitlab.com/PixysOS-fourteen/vendor_nothing_camera.git  -b fourteen-v2 vendor/nothing/camera
-git clone https://github.com/PixysOS-Devices/packages_apps_PixysGlyph.git -b fourteen-v2 packages/apps/PixysGlyph
+git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325.git -b fourteen-v3 kernel/nothing/sm7325
+git clone https://github.com/PixysOS-Devices/packages_apps_PixysGlyph.git -b fourteen-v3 packages/apps/PixysGlyph
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_nothing_camera.git -b uvite vendor/nothing/camera
